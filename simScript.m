87e633b80f67100c863325aa6ca2ff07ce69b1d3
Aamir Ahmed Khan <akhan3@nd.edu>
2012-02-15 20:02:09 -0500

    Improved the validate file

--- simScript.m
+++ simScript.m
@@ -18,17 +18,18 @@ clear
     sp.startTimeStamp = clock;  % record the start wall-clock-time
     sp.finishedWithSuccess = 0; % initially mark as unsuccessful
     % simulation time
-    sp.tf = 1e-9;
-    sp.dt = 1e-13;
-    sp.t = [0:sp.dt:sp.tf];
-    sp.Nt = length(sp.t);
+    sp.ti = 0;  % initial time
+    sp.tf = 1e-9;   % final time
+    sp.dt = 1e-13;  % time step
+    sp.t = [sp.ti:sp.dt:sp.tf]; % time array
+    sp.Nt = length(sp.t)+1;   % number of time points
     % number of the dots
         % Nx*Ny MUST NOT exceed (5000)^2
         % Total GPU memory = 2.8177e+09 Bytes
         % Each dot requires 3*4 Bytes
         % 2.8177e+09/12/10 to accommodate M(t),M(t+1)Hext,... on the GPU
-    sp.Ny = 50;     % #rows of dots in the plane
-    sp.Nx = 50;     % #columns of dots in the plane
+    sp.Ny = 100;     % #rows of dots in the plane
+    sp.Nx = 100;     % #columns of dots in the plane
     % material parametrs
         % TODOL: spatially varying
     sp.Ms = 8.6e5;
@@ -51,8 +52,10 @@ clear
     %                   r = row coordinate
     %                   c = column coordinate in plane
     %                   t = time coordinate
-    M = zeros(3,sp.Ny,sp.Nx,sp.Nt);     % can be huge in size
-    Hext = zeros(size(M));              % same as size of M
+    % It is better to define them as single precision,
+    % otherwise they will be converted by the validateSimParam routine anyway.
+    M = single(zeros(3,sp.Ny,sp.Nx,sp.Nt)); % can be huge in size
+    Hext = single(zeros(size(M)));          % same size as of M
 
 
 %===============================================================================
@@ -73,32 +76,35 @@ clear
         theta = 40 * pi/180 .* ones(sp.Ny, sp.Nx);
         phi   = 2*pi .* rand(sp.Ny, sp.Nx);
     end
-    ic.M = zeros(3,sp.Ny,sp.Nx);
-    ic.M(1,:,:) = sp.Ms .* sin(theta) .* cos(phi);
-    ic.M(2,:,:) = sp.Ms .* sin(theta) .* sin(phi);
-    ic.M(3,:,:) = sp.Ms .* cos(theta);
+    initCond.M = zeros(3,sp.Ny,sp.Nx);
+    initCond.M(1,:,:) = sp.Ms .* sin(theta) .* cos(phi);
+    initCond.M(2,:,:) = sp.Ms .* sin(theta) .* sin(phi);
+    initCond.M(3,:,:) = sp.Ms .* cos(theta);
+    % assign initCond.M to M(r,t=1) - DON'T FORGET!
+    M(:,:,:,1) = initCond.M;
 
 
 %===============================================================================
 %% Boundary condtions for M
 %===============================================================================
-    bc.Mtop = zeros(3,sp.Nx);    % +x top
-    bc.Mbot = zeros(3,sp.Nx);    % -x bottom
-    bc.Mrig = zeros(3,sp.Ny);    % +y right
-    bc.Mlef = zeros(3,sp.Ny);    % -y left
-    sp.bc = bc; % Must add bc to the simParam structure - IMPORTANT!
+    sp.boundCond.Mtop = zeros(3,sp.Nx);    % +x top
+    sp.boundCond.Mbot = zeros(3,sp.Nx);    % -x bottom
+    sp.boundCond.Mrig = zeros(3,sp.Ny);    % +y right
+    sp.boundCond.Mlef = zeros(3,sp.Ny);    % -y left
 
 
 %===============================================================================
 %% Validate the parameters - VERY IMPORTANT!
 %===============================================================================
 fprintf('INFO: Starting simulation...\n');
+fprintf('INFO: Verifying simulation parameters...\n');
 [success,sp,M,Hext] = validateSimParam(sp,M,Hext);
 if success == 0
     fprintf('ERROR: Simulation cannot start due to bad parameters.\n');
     return;
 else
-    fprintf('INFO: All simulation parameters have been verified.\n'); sp
+    fprintf('INFO: All simulation parameters have been verified.\n');
+    sp
 end
 
 
@@ -106,7 +112,7 @@ end
 %% Time marching
 %===============================================================================
 fprintf('INFO: Time marching for %d points...\n', sp.Nt);
-M(:,:,:,1) = ic.M;  % assign ic.M to M(t=1)
+M(:,:,:,1) = initCond.M;  % assign initCond.M to M(t=1)
 % H = zeros(3,sp.Ny,sp.Nx); % could pre-allocate Hfield to save time
 for k = 1:length(sp.t)-1   % solve ODE for all time points but last
     fprintf('INFO: %.1f%% t(%d)=%gs: ', ...
@@ -114,7 +120,7 @@ for k = 1:length(sp.t)-1   % solve ODE for all time points but last
     tic;    % code instrumentation
     M(:,:,:,k+1) = odeStepComp(sp, M(:,:,:,k), Hext(:,:,:,k));
     timeTaken = toc;
-    fprintf('ODE step executed in %.2g ms runtime\n', timeTaken*1000);
+    fprintf('ODE step executed in %.2f ms runtime\n', timeTaken*1000);
 end
 
 %===============================================================================
@@ -123,11 +129,12 @@ end
 sp.stopTimeStamp = clock;   % record the stop wall-clock-time
 sp.runTime = datevec(datenum(sp.stopTimeStamp) - datenum(sp.startTimeStamp));
 sp.finishedWithSuccess = 1; % now mark as a successful finish
-fprintf('INFO: simParam structure\n'); sp
+fprintf('INFO: simParam structure\n');
+sp
 fprintf('INFO: Simulation Finished!\n');
 
 
 %===============================================================================
 %% Post processing (optional): save the data and visualize the result
 %===============================================================================
-% animateDots
+animateDots
--- src/validateSimParam.m
+++ src/validateSimParam.m
@@ -2,87 +2,77 @@ function [success,sp,M,Hext] = validateSimParam(sp,M,Hext)
 
     success = 1;
 
-    %% Default sp (simParam structure)
+%===============================================================================
+%% Default sp (simParam structure)
+%===============================================================================
+    % Book-keeping information
     spDefault.simName = 'Untitled Simulation';
+    spDefault.startTimeStamp = clock;
+    spDefault.stopTimeStamp = inf * clock;
+    spDefault.runTime = inf * clock;
+    spDefault.finishedWithSuccess = int32(0);
+    % Simulation time
+    spDefault.ti = single(0);
     spDefault.tf = single(1e-11);
     spDefault.dt = single(1e-13);
-    spDefault.Nt = int32(floor(spDefault.tf/spDefault.dt) + 1);
+    spDefault.t = single([spDefault.ti:spDefault.dt:spDefault.tf]);
+    spDefault.Nt = int32(length(spDefault.t));
+    % number of the dots
     spDefault.Ny = int32(20);
     spDefault.Nx = int32(20);
+    % material parametrs
     spDefault.Ms = single(8.6e5);
     spDefault.gamma = single(2.21e5);
     spDefault.alpha = single(0.05);
     spDefault.cCoupl = single([-.2 -.2 -.2]);
     spDefault.cDemag = single([.4 .4 .2]);
+    % ODE Solver selection
     spDefault.useRK4 = int32(0);
     spDefault.useGPU = int32(0);
+    % Boundary condition defaults to ZERO
+    spDefault.boundCond.Mtop = single(zeros(3,sp.Nx));    % +x top
+    spDefault.boundCond.Mbot = single(zeros(3,sp.Nx));    % -x bottom
+    spDefault.boundCond.Mrig = single(zeros(3,sp.Ny));    % +y right
+    spDefault.boundCond.Mlef = single(zeros(3,sp.Ny));    % -y left
 
 
-    %% valiade sp (simParam structure)
-    if ~fieldExists(sp,'simName')
-        sp.simName = spDefault.simName;
+%===============================================================================
+%% Valiade sp (simParam structure)
+%===============================================================================
+    fnames = fieldnames(spDefault);
+    for i = 1:length(fnames)
+        if ~fieldExists(sp, fnames(i))
+            fprintf('WARNING: sp.%s field does not exist, so creating with default value\n', char(fnames(i)));
+            cmd = sprintf('sp.%s = spDefault.%s;', char(fnames(i)), char(fnames(i)));
+            eval(cmd);
+        end
     end
 
-    if ~fieldExists(sp,'tf')
-        sp.tf = spDefault.tf;
+    % sanity check for sp.t and sp.Nt
+    % TODO: sanity check for time array
+    if (sp.Nt ~= length(sp.t))
+        fprintf('WARNING: sp.Nt does not match the length of time array. Fixed it\n');
+        sp.Nt = int32(length(sp.t));
     end
 
-    if ~fieldExists(sp,'dt')
-        sp.dt = spDefault.dt;
-    end
+    %if ~fieldExists(sp,'Ms')
+        %sp.Ms = spDefault.Ms;
+    %elseif sp.Ms == 0
+        %fprintf('Ms cannot have a value of %g [A/m]\n', sp.Ms);
+        %success = 0;
+    %end
 
-    if ~fieldExists(sp,'Nt') || ~(sp.Nt == floor(sp.tf/sp.dt) + 1)
-        sp.Nt = int32(floor(sp.tf/sp.dt) + 1);
-    end
 
-    if ~fieldExists(sp,'Nx')
-        sp.Nx = spDefault.Nx;
-    end
-
-    if ~fieldExists(sp,'Ny')
-        sp.Ny = spDefault.Ny;
-    end
-
-    if ~fieldExists(sp,'Ms')
-        sp.Ms = spDefault.Ms;
-    elseif sp.Ms == 0
-        fprintf('Ms cannot have a value of %g [A/m]\n', sp.Ms);
-        success = 0;
-    end
-
-    if ~fieldExists(sp,'gamma')
-        sp.gamma = spDefault.gamm;
-    elseif sp.gamma == 0
-        fprintf('gamma cannot have a value of %g\n', sp.gamma);
-        success = 0;
-    end
-
-    if ~fieldExists(sp,'alpha')
-        sp.alpha = spDefault.alpha;
-    end
-
-    if ~fieldExists(sp,'cCoupl')
-        sp.cCoupl = spDefault.cCoupl;
-    end
-
-    if ~fieldExists(sp,'cDemag')
-        sp.cDemag = spDefault.cDemag;
-    end
-
-    if ~fieldExists(sp,'useRK4')
-        sp.useRK4 = spDefault.useRK4;
-    end
-
-    if ~fieldExists(sp,'useGPU')
-        sp.useGPU = spDefault.useGPU;
-    end
-
-    %% convert everything to single precision or 32-bit integer
+%===============================================================================
+%% convert everything to single precision or 32-bit integer
+%===============================================================================
+    sp.ti = single(sp.ti);
     sp.tf = single(sp.tf);
     sp.dt = single(sp.dt);
+    sp.t = single(sp.t);
     sp.Nt = int32(sp.Nt);
-    sp.Nx = int32(sp.Nx);
     sp.Ny = int32(sp.Ny);
+    sp.Nx = int32(sp.Nx);
     sp.Ms = single(sp.Ms);
     sp.gamma = single(sp.gamma);
     sp.alpha = single(sp.alpha);
@@ -90,19 +80,30 @@ function [success,sp,M,Hext] = validateSimParam(sp,M,Hext)
     sp.cDemag = single(sp.cDemag);
     sp.useRK4 = int32(sp.useRK4);
     sp.useGPU = int32(sp.useGPU);
-    % boundary conditions
-    sp.bc.Mtop = single(sp.bc.Mtop);
-    sp.bc.Mbot = single(sp.bc.Mbot);
-    sp.bc.Mrig = single(sp.bc.Mrig);
-    sp.bc.Mlef = single(sp.bc.Mlef);
+    sp.boundCond.Mtop = single(sp.boundCond.Mtop);
+    sp.boundCond.Mbot = single(sp.boundCond.Mbot);
+    sp.boundCond.Mrig = single(sp.boundCond.Mrig);
+    sp.boundCond.Mlef = single(sp.boundCond.Mlef);
     % M(r,t) and Hext(r,t) bulky arrays
-    M = single(M);
-    Hext = single(Hext);
+    if strcmp(class(M), 'single') == 0
+        fprintf('INFO: converting M to single precision... ');
+        M = single(M);
+        fprintf('done\n');
+    end
+    if strcmp(class(Hext), 'single') == 0
+        fprintf('INFO: converting Hext to single precision... ');
+        Hext = single(Hext);
+        fprintf('done\n');
+    end
 end
 
 
 
+
+
+%===============================================================================
 %% check if a field exists in a structure
+%===============================================================================
 function present = fieldExists(struct, fname)
     present = any(strcmp(fname,fieldnames(struct)));
 end
